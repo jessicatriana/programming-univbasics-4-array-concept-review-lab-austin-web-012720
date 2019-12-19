27ef0fad9777ff8e951d296be5d5bbfe6ca5ce82
Jessica Triana <jessica.triana.tx@gmail.com>
2019-12-19 17:03:47 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -9,7 +9,12 @@ end
 
 
 def find_max_value(array)
-  # Add your solution here
+counter = 0 
+
+while counter<array.length do
+  
+  return #highest Integer
+  count
 end
 
 def find_min_value(array)
