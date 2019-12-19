aad7157be4c6dd44afdbdd290764f58652804031
Jessica Triana <jessica.triana.tx@gmail.com>
2019-12-19 16:53:11 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,13 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  counter = 0 
+  
+  while counter < array.length do
+    array.index(value_to_find)
+    counter +=1
 end
+return index
+end
+
 
 def find_max_value(array)
   # Add your solution here
